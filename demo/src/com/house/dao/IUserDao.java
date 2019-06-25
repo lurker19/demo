d6bc5abd8a6c0526f9ee2510c58b775e03e140bc
Lz <Lz@Csu>
2019-06-25 15:54:14 +0800

    test

--- demo/src/com/house/dao/IUserDao.java
+++ demo/src/com/house/dao/IUserDao.java
@@ -4,6 +4,12 @@ import com.house.vo.User;
 
 public interface IUserDao {
 
+	/**
+	 * ÓÃ»§µÇÂ½
+	 * @param name
+	 * @param pwd
+	 * @return
+	 */
 	public boolean login(String name , String pwd);
 	
 	public boolean editorUser(User uer) ;
